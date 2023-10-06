86926e9c58967d00e8bd4c6280b633f0caf29643
Möri Simon <simon.moeri@schweizer-electronic.com>
2023-10-06 13:07:59 +0200

    restructured filesystem

---
+++ src/PID.h
@@ -0,0 +1,31 @@
+/*
+  PID.h - Library for a simple PID controller
+  Created by Simon Moeri, October 6, 2023.
+  Released into the public domain.
+*/
+
+#ifndef PID_H
+#define PID_H
+
+class PID
+{
+  public:
+      PID(float proportional_Constant, float integral_Constant, float derivative_Constant, bool boolean_Controller);
+      void begin(float setpoint);
+      void set_constants(float proportional_Constant, float integral_Constant, float derivative_Constant, bool boolean_Controller);
+      void set_setpoint(float setpoint);
+      float calculate_controller_output(float measured_Value);
+
+  private:
+      virtual unsigned long determine_time();
+      float kp = 1.0; //proportional constant
+      float ki = 1.0; //integral constant
+      float kd = 1.0; //derivative constant
+      bool controller_Type = true; //if this is set to TRUE, controller output will be either 0 or 1, otherwise float
+      unsigned long timepoint = 0; //storage for the time between calculations
+      float setpoint = 0.0; //desired regulated value
+      float measurement = 0.0; //actual measured value
+      float error = 0.0; //difference between setpoint and measurement
+      float control_Output = 0.0; //in case of boolean controller this will be either 1 or 0 otherwise returns the float     
+};
+#endif
---
+++ src/PID_Arduino.h
@@ -0,0 +1,20 @@
+/*
+  PID_Arduino.h - Library to implement a simple PID controller on an Arduino
+  Created by Simon Moeri, October 6, 2023.
+  Released into the public domain.
+*/
+#ifndef PID_ARDUINO_H
+#define PID_ARDUINO_H
+#include "PID.h"
+//#include "Arduino.h"
+
+class PID_arduino : public PID
+{
+  public:
+    PID_arduino(float proportional_Constant, float integral_Constant, float derivative_Constant, bool boolean_Controller) : PID(proportional_Constant, integral_Constant, derivative_Constant, boolean_Controller){}
+  private:
+    long unsigned determine_time(){
+          return 10;
+      }
+};
+#endif
