5b41f0a341117ce773e6d513200f3d0521a8e8ef
Simon Moeri <simon.moeri@students.fhnw.ch>
2023-10-06 23:10:40 +0200

    first commit of full example, needs testing

--- examples/Reflow_Oven/Reflow_Oven.ino
+++ examples/Reflow_Oven/Reflow_Oven.ino
@@ -4,29 +4,134 @@
 #include "PID.h"
 #include "PID_Arduino.h"
 
-enum solder_state{
-  inactive,
-  preheat,
-  soak,
-  reflow,
-  plateau,
-  cooling
+enum solder_state {
+  inactive_state,
+  preheat_state,
+  soak_state,
+  reflow_state,
+  plateau_state,
+  cooling_state
 };
 
-solder_state state = inactive;
-PID_arduino controller(1,0.1,0.2,true);
-bool Heater = false;
-float temp = 25;
+#define time_between_decisions 500  //ms
+#define second 1000                 //ms
+#define preheat_step 3 * time_between_decisions / second
+#define preheat_limit 150
+#define wait_time_soak second / time_between_decisions * 90
+#define reflow_step 2 * time_between_decisions / second
+#define reflow_limit 245
+#define plateau_time 40  //from 40 to 80 possible
+#define wait_time_plateau second / time_between_decisions* plateau_time
+#define cooling_step -4 * time_between_decisions / second¨
+#define cooling_limit 35
 
+solder_state state = inactive_state;
+PID_arduino PID_controller(1, 0.1, 0.2, true);
+bool heater_relais = false;
+uint8_t NTC_pin = A0;
+uint8_t relais_pin = 2;
+uint8_t button_pin = 3;
+int NTC_voltage = 0;
+float temperature = 24.0;
+float desired_temperature = 24.0;
+
+unsigned long run_time = 0;
+int time_counter = 0;
+bool running = false;
 
 void setup() {
-  controller.begin(25);
+  Serial.begin(9600);
+  pinMode(relais_pin, OUTPUT);
+  pinMode(button_pin, INPUT);
+  PID_controller.begin(desired_temperature);
 }
 
 void loop() {
+  if ((run_time = millis()) % time_between_decisions == 0) {  //check if this is not skipped sometimes
+    Serial.println(run_time);
+    state_machine(state);
+    if (!running) {
+      Serial.println("Ready");
+    }
+    PID_controller.set_setpoint(desired_temperature);
+    Serial.println(temperature);
+    Serial.println(heater_relais);
+  }
+  //measure temperature;
+  heater_relais = PID_controller.calculate_controller_output_bool(temperature);
+  digitalWrite(relais_pin, heater_relais);
+}
 
-  Heater = controller.calculate_controller_output_bool(temp);
+void state_machine(solder_state state) {
+  switch (state) {
+    case inactive_state:
+      if (digitalRead(button_pin) == 1) {
+        state = preheat_state;
+        running = true;
+      }
+      break;
+    case preheat_state:
+      preheat();
+      break;
+    case soak_state:
+      soak();
+      break;
+    case reflow_state:
+      reflow();
+      break;
+    case plateau_state:
+      plateau();
+      break;
+    case cooling_state:
+      cooling();
+      break;
+  }
+}
 
+void preheat() {
+  if (temperature >= preheat_limit) {
+    state = soak_state;
+  }
+  if (desired_temperature <= preheat_limit) {
+    desired_temperature += preheat_step;
+  }
+}
+
+void soak() {
+  if (time_counter >= wait_time_soak) {
+    state = reflow_state;
+    time_counter = 0;
+  } else {
+    time_counter += 1;
+  }
+}
+
+void reflow() {
+  if (temperature >= reflow_limit) {
+    state = plateau_state;
+  }
+  if (desired_temperature <= reflow_limit) {
+    desired_temperature += reflow_step;
+  }
+}
+
+void plateau() {
+  if (time_counter >= wait_time_plateau) {
+    state = cooling_state;
+    time_counter = 0;
+  } else {
+    time_counter += 1;
+  }
+}
+
+void cooling() {
+  if (temperature <= cooling_limit) {
+    state = inactive_state;
+    running = false;
+  }
+  if (desired_temperature <= cooling_limit) {
+    desired_temperature -= reflow_step;
+  }
 }
 
 /*
